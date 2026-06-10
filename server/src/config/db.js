76458e8ad2a0f50d755e23e34d305c25ac270e38
Manijitya30 <Manijitya30@gmail.com>
2026-06-10 20:57:13 +0530

    deploy changes

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,16 +1,6 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+const db = mysql.createPool(process.env.MYSQL_URL);
 
 function ensureProductImagesColumn() {
   db.query(
